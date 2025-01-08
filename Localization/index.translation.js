49ab2d517e8dcbfef9f79b001465dc0dca3ba8a2
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-01-08 22:23:08 +0700

    progressing in homepage

--- Localization/index.translation.js
+++ Localization/index.translation.js
@@ -17,8 +17,57 @@ const translations = {
       address: "Address",
       callus: "Call us",
     },
-    // Translation Data for homepage
+    // Translation data for homepage
     homepage: {
+      whyus: {
+        whyusTitle: "Why us?",
+        icon1title: "Fresh Fish Supply",
+        icon1text:
+          "We guarantee fresh fish for local and overseas markets that meet international quality standards",
+        icon2title: "Cold Storage Solutions",
+        icon2text:
+          "Our modern facilities ensure your fish remains fresh and preserved for longer durations",
+        icon3title: "Export Expertise",
+        icon3text:
+          "With our experience in international trade, we handle the logistics to deliver your products worldwide",
+        icon4title: "Fish Processing",
+        icon4text:
+          "From cleaning to packaging, we provide comprehensive fish processing services tailored to your",
+        icon5title: "Sustainable Practices",
+        icon5text:
+          "We are committed to eco-friendly methods, ensuring minimal impact on the environment while maximizing productivity",
+        icon6title: "Customer Support",
+        icon6text:
+          "Our dedicated team is always ready to assist you, ensuring smooth operations and excellent service",
+      },
+      visimisi: {
+        buttonSection: {
+          misi: "Our Mission",
+          visi: "Our Vision",
+        },
+        misi: {
+          poin1title: "Enhancing Sustainable Fisheries Production:",
+          poin1text:
+            "Develop and implement sustainable fishing practices that prioritize marine resource conservation and ecosystem preservation to meet long-term market demands",
+          poin2title: "Innovating in Fisheries Technology:",
+          poin2text:
+            "Adopt the latest technology in aquaculture, processing, and distribution to boost efficiency, improve product quality, and enhance competitiveness",
+          poin3title: "Empowering Coastal Communities:",
+          poin3text:
+            "Support fishermen and fish farmers through training, mentoring, and improved market access to enhance their livelihoods and create job opportunities",
+          poin4title: "Ensuring Product Quality and Safety:",
+          poin4text:
+            "Produce high-quality fishery products that meet strict food safety standards, with a strong focus on cleanliness and halal compliance",
+          poin5title: "Promoting Education and Environmental Awareness:",
+          poin5text:
+            "Raise public awareness about the importance of preserving marine resources and encourage responsible consumption to ensure the sustainability of fisheries",
+        },
+        visi: "Becoming a leading company that contributes to global food security through sustainable, high-quality, and environmentally friendly fish production while improving the welfare of coastal communities",
+      },
+      newsPage: {
+        newsHeader: "News",
+        newsSubheader: "Latest news from us",
+      },
       title: "Tridaya Pasifik KSO",
       subtitle: "Providing fresh and high-quality fish for your needs",
       description:
@@ -29,22 +78,36 @@ const translations = {
         "We take pride in delivering premium seafood that meets your expectations. Order from us today and experience the difference",
       likeourproduct: "Do you like our product?",
       ordernowlink: "Let's order it now!",
-      weofferservice: "We offer services",
-      ourservices: "Our factory services",
+      // weofferservice: "We offer services",
+      // ourservices: "Our factory services",
+
       ourproduct: "Our product",
       productlist: "Check out the products we offer",
       mediawelcome: "Welcome to Tridaya Pasifik KSO",
       mediaSubheader: "We offer you quality and fresh fish product",
       mediadescription:
         "We offer you premium quality, fresh fish products that are carefully sourced and handled with the utmost care. Our commitment to freshness ensures that every product meets the highest standards, providing you with a taste that’s both delicious and fresh",
-      vision:
-        "Becoming a leading company that contributes to global food security through sustainable, high-quality, and environmentally friendly fish production while improving the welfare of coastal communities",
+
       emailusHeader: "Email us!",
       orderfish: "Order your fish now!",
+      socialMedia: {
+        title: "SOCIAL MEDIA",
+        subtitle: "Follow our social media",
+      },
     },
     footer: {
-      copyright: "© 2025 Our Company. All rights reserved.",
-      contact: "Contact us at info@ourwebsite.com",
+      contactInformation: "Contact information",
+      businessHours: {
+        title: "Business Hours",
+        openingDays: {
+          title: "Opening Days:",
+          text: "Monday – Friday : 9:00 AM to 17:00 PM",
+        },
+        vacation: {
+          title: "Vacations:",
+          text: "Saturday and Sunday, as well as all official Holidays",
+        },
+      },
     },
   },
   // indonesia translations
@@ -64,9 +127,59 @@ const translations = {
       address: "Alamat",
       callus: "Hubungi kami",
     },
-    // Translation Data for homepage
+    // Translation data for homepage
 
     homepage: {
+      whyus: {
+        whyusTitle: "Mengapa kami?",
+        icon1title: "Produk Ikan Segar",
+        icon1text:
+          "Kami menjamin ikan segar untuk pasar lokal dan luar negeri yang memenuhi kualitas standar internasional",
+        icon2title: "Solusi Penyimpanan Produk Frozen",
+        icon2text:
+          "Fasilitas modern kami memastikan ikan tetap segar dan terjaga untuk durasi yang lebih lama",
+        icon3title: "Keahlian Ekspor",
+        icon3text:
+          "Dengan pengalaman dalam perdagangan internasional, kami menangani logistik untuk mengirim produk Anda ke seluruh dunia",
+        icon4title: "Pengolahan Ikan",
+        icon4text:
+          "Dari pembersihan hingga pengemasan, kami menyediakan layanan pengolahan ikan yang komprehensif yang disesuaikan dengan kebutuhan Anda",
+        icon5title: "Ramah Lingkungan",
+        icon5text:
+          "Kami berkomitmen pada metode ramah lingkungan, memastikan dampak minimal terhadap lingkungan sambil memaksimalkan produktivitas",
+        icon6title: "Dukungan Pelanggan",
+        icon6text:
+          "Tim kami selalu siap untuk membantu anda agar pelayanan kami maksimal dan sesuai dengan yang anda inginkan",
+      },
+
+      visimisi: {
+        buttonSection: {
+          misi: "Misi Kami",
+          visi: "Visi Kami",
+        },
+        misi: {
+          poin1title: "Meningkatkan Produksi Perikanan Berkelanjutan:",
+          poin1text:
+            "Mengembangkan dan menerapkan praktik perikanan yang berkelanjutan, mengutamakan konservasi sumber daya laut dan pemeliharaan ekosistem agar dapat memenuhi kebutuhan pasar jangka panjang",
+          poin2title: "Inovasi dalam Teknologi Perikanan:",
+          point2text:
+            "Menggunakan teknologi terkini dalam proses budidaya, pengolahan, dan distribusi untuk meningkatkan efisiensi, kualitas produk, dan daya saing",
+          poin3title: "Pemberdayaan Masyarakat Pesisir:",
+          poin3text:
+            "Memberdayakan komunitas nelayan dan petani ikan dengan pelatihan, pendampingan, dan akses ke pasar, sehingga dapat meningkatkan kesejahteraan mereka serta menciptakan lapangan pekerjaan",
+          poin4title: "Kepedulian terhadap Kualitas dan Keamanan Produk:",
+          poin4text:
+            "Menjamin produk perikanan yang dihasilkan memiliki kualitas tinggi dan memenuhi standar keamanan pangan yang ketat, dengan mengutamakan kebersihan dan kehalalan",
+          poin5title: "Edukasi dan Kesadaran Lingkungan:",
+          poin5text:
+            "Mengedukasi konsumen dan masyarakat tentang pentingnya menjaga kelestarian sumber daya alam laut, serta mempromosikan pola konsumsi yang bertanggung jawab terhadap keberlanjutan sumber daya perikanan",
+        },
+        visi: "Menjadi perusahaan terkemuka yang berkontribusi pada ketahanan pangan global melalui produksi perikanan yang berkelanjutan, berkualitas tinggi, ramah lingkungan, serta meningkatkan kesejahteraan masyarakat pesisir",
+      },
+      newsPage: {
+        newsHeader: "Berita",
+        newsSubheader: "Berita terkini dari kami",
+      },
       title: "Tridaya Pasifik KSO",
       subtitle: "Menyediakan ikan segar dan berkualitas untuk kebutuhan anda",
       description:
@@ -77,8 +190,9 @@ const translations = {
         "Kami dengan senang hati menyediakan makanan laut premium yang pasti memuaskan Anda. Ayo! pesan sekarang dan rasakan bedanya bersama kami!",
       likeourproduct: "Anda suka produk kami?",
       ordernowlink: "Pesan sekarang juga!",
-      weofferservice: "Kami menawarkan pelayanan",
-      ourservices: "Pelayanan pabrik kami",
+      // weofferservice: "Kami menawarkan pelayanan",
+      // ourservices: "Pelayanan pabrik kami",
+
       ourproduct: "Produk kami",
       productlist: "Lihat produk yang kami tawarkan",
       mediawelcome: "Selamat datang di Tridaya Pasifik KSO",
@@ -86,14 +200,29 @@ const translations = {
         "Kami menawarkan anda produk ikan yang segar dan berkualitas",
       mediadescription:
         "Kami menawarkan produk ikan berkualitas tinggi dan segar yang dipilih dengan teliti dan ditangani dengan perhatian penuh. Komitmen kami terhadap kesegaran memastikan setiap produk memenuhi standar tertinggi, memberikan Anda rasa yang lezat dan segar",
-      vision:
-        "Menjadi perusahaan terkemuka yang berkontribusi pada ketahanan pangan global melalui produksi perikanan yang berkelanjutan, berkualitas tinggi, ramah lingkungan, serta meningkatkan kesejahteraan masyarakat pesisir",
+
       emailusHeader: "Hubungi kami!",
       orderfish: "Pesan produk kami sekarang!",
+
+      socialMedia: {
+        title: "MEDIA SOSIAL",
+        subtitle: "Ikuti media sosial kami",
+      },
     },
+
     footer: {
-      copyright: "© 2025 Our Company. All rights reserved.",
-      contact: "Contact us at info@ourwebsite.com",
+      contactInformation: "Informasi Kontak",
+      businessHours: {
+        title: "Jadwal Kerja",
+        openingDays: {
+          title: "Hari Kerja:",
+          text: "Senin - Jumat: 09:00 AM - 17:00 PM ",
+        },
+        vacation: {
+          title: "Hari Libur:",
+          text: "Sabtu, Minggu dan hari libur nasional",
+        },
+      },
     },
   },
 };
