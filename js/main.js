1e1d0f2bb843f46247d56e05547e29458e36a3f5
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-01-20 14:44:02 +0700

    added new owl carousel script targeting product list class

--- js/main.js
+++ js/main.js
@@ -79,6 +79,57 @@
         },
       },
     });
+
+    $(".carousel-productlist").owlCarousel({
+      center: true,
+      autoplay: false,
+      loop: true,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: false,
+      navText: [
+        '<span class="ion-ios-arrow-back">',
+        '<span class="ion-ios-arrow-forward">',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+
+    $(".carousel-productlist-auto").owlCarousel({
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 3500,
+      loop: true,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: false,
+      navText: [
+        '<span class="ion-ios-arrow-back">',
+        '<span class="ion-ios-arrow-forward">',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
   };
   carousel();
 
