3dc0de18bafb04fee0ccd266ad4ff89b65bc2329
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-02-17 17:13:47 +0700

    rework ton of thing

--- js/itemSlider.js
+++ js/itemSlider.js
@@ -139,19 +139,69 @@ document.addEventListener("DOMContentLoaded", function () {
       1024: { slidesPerView: 4 },
     },
   });
+  new Swiper(".opinion-slider", {
+    lazy: true,
+    nav: false,
+    speed: 1000,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    rewind: true,
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true,
+    },
+    breakpoints: {
+      500: { slidesPerView: 2 },
+      1000: { slidesPerView: 3 },
+    },
+  });
+
+  new Swiper(".client-slider", {
+    lazy: true,
+    nav: false,
+    speed: 1000,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    rewind: true,
+    autoplay: {
+      delay: 2500,
+      pauseOnMouseEnter: false,
+    },
+    breakpoints: {
+      400: { slidesPerView: 2 },
+      750: { slidesPerView: 3 },
+      1000: { slidesPerView: 4 },
+    },
+  });
 
   // main product slider description
 
-  new Swiper(".product-desc-slider", {
+  const productDescSwiper = new Swiper(".product-desc-slider", {
     scrollbar: {
       el: ".swiper-scrollbar",
       hide: true,
     },
-    speed: 1500,
+    speed: 1300,
     slidesPerView: 1,
     autoplay: {
-      delay: 5000,
-      pauseOnMouseEnter: false,
+      delay: 4000,
+      pauseOnMouseEnter: true,
     },
+  });
+
+  const navButtons = document.querySelectorAll(".nav-btn-slideTo");
+  const slides = document.querySelectorAll(".swiper-slide-slideTo");
+
+  navButtons.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1); // Remove #
+
+      slides.forEach((slide, index) => {
+        if (slide.id === targetId) {
+          productDescSwiper.slideTo(index); // Navigate to the correct slide
+        }
+      });
+    });
   });
 });
