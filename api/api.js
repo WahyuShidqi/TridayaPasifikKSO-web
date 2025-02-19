38eb30ef79923fededdd8992b5aa37dfc8cec6af
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-02-19 14:35:51 +0700

    Added certification inside footer and adjusted the way the navbar look

--- api/api.js
+++ api/api.js
@@ -1,39 +1,3 @@
-// document
-//   .getElementById("appointment-form")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     const name = document.getElementById("name").value;
-//     const location = document.getElementById("location").value;
-//     const product = document.getElementById("product").value;
-//     const date = document.getElementById("date").value;
-//     const userEmail = document.getElementById("email").value;
-//     const message = document.getElementById("message").value;
-
-//     // Check if all fields are filled out (basic validation)
-//     if (!name || !location || !product || !date || !userEmail || !message) {
-//       alert("All fields must be filled out!");
-//       return;
-//     }
-
-//     // Send data to the backend
-//     fetch("https://tridaya-mail-server.vercel.app/api/send-email", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         name,
-//         location,
-//         product,
-//         date,
-//         userEmail,
-//         message,
-//       }),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => alert(data.message))
-//       .catch((error) => alert("Error: " + error));
-//   });
-
 document
   .getElementById("appointment-form")
   .addEventListener("submit", function (event) {
@@ -42,11 +6,18 @@ document
     const name = document.getElementById("name").value;
     const location = document.getElementById("location").value;
     const product = document.getElementById("product").value;
-    const date = document.getElementById("date").value;
+    const phoneNumber = document.getElementById("phone-number").value;
     const userEmail = document.getElementById("email").value;
     const message = document.getElementById("message").value;
 
-    if (!name || !location || !product || !date || !userEmail || !message) {
+    if (
+      !name ||
+      !location ||
+      !product ||
+      !phoneNumber ||
+      !userEmail ||
+      !message
+    ) {
       alert("All fields must be filled out!");
       return;
     }
@@ -58,7 +29,7 @@ document
         name,
         location,
         product,
-        date,
+        phoneNumber,
         userEmail,
         message,
       }),
