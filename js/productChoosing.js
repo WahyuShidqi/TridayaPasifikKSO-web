a194d772e4e2dc9552fa7cfe4fc1d8697758b9aa
Wahyu Shidqi <wahyushidqi550@gmail.com>
2025-04-16 14:55:44 +0700

    clean up the codes --js, added more static options -html

--- js/productChoosing.js
+++ js/productChoosing.js
@@ -2,6 +2,7 @@ const products = document.querySelectorAll(".product");
 const selector = document.querySelector("#product");
 const goBackBtn = document.querySelector("#gobackbutton");
 const body = document.querySelector("body");
+const staticOptions = selector.querySelectorAll("option");
 
 let selectedProductId = "";
 
@@ -25,28 +26,41 @@ products.forEach((product) => {
     //storing unique product text id so goback btn can use it universally
     selectedProductId = this.id;
 
+    //assing value to selector when product clicked
+
+    //comparing if the value coming from the product is available in the options
+    const isValueAvailable = Array.from(staticOptions).some((staticOption) => {
+      return staticOption.value === this.dataset.value;
+    });
+
     //removing additional added option that are more than one
-    document.querySelectorAll(".new-option").forEach((opt) => opt.remove());
+    if (!isValueAvailable) {
+      document.querySelectorAll(".new-option").forEach((opt) => opt.remove());
 
-    //create new option and make select tag use its value
-    const newOptions = document.createElement("option");
-    newOptions.classList.add("new-option");
-    newOptions.value = this.dataset.value;
-    newOptions.innerText = this.dataset.value;
+      //create new option and make select tag use its value
+      const newOptions = document.createElement("option");
+      newOptions.classList.add("new-option");
+      newOptions.value = this.dataset.value;
+      newOptions.innerText = this.dataset.value;
 
-    selector.appendChild(newOptions);
+      selector.appendChild(newOptions);
+      selector.value = this.dataset.value;
+    } else {
+      document.querySelectorAll(".new-option").forEach((opt) => opt.remove());
+    }
+
     selector.value = this.dataset.value;
 
     //go to email form
     body.classList.remove("scroll-5vh");
     body.classList.add("scroll-20vh");
     product.href = "#product";
-
-    //go back to clicked item menu
-    goBackBtn.addEventListener("click", function () {
-      body.classList.add("scroll-5vh");
-      body.classList.remove("scroll-20vh");
-      this.href = `#${product.id}`;
-    });
   });
+});
+
+//go back to clicked item menu
+goBackBtn.addEventListener("click", function () {
+  body.classList.add("scroll-5vh");
+  body.classList.remove("scroll-20vh");
+  this.href = `#${selectedProductId}`;
 });
